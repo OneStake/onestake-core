d5f30fc101371dbf510865ec2177c5d20601d1a2
SmartCoding51 <topstar51@outlook.com>
2022-05-13 02:55:28 +0900

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,8 +22,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
+    hardhat: {
+      forking: { // mainnet fork
+        enabled: true,
+        url: process.env.MAINNET_URL || "",
+      }
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
