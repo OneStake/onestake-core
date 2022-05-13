35bd589b828953c4493f2ad819d56d84e0c4a33a
SmartCoding51 <topstar51@outlook.com>
2022-05-14 01:54:35 +0900

    update contracts structure

---
+++ contracts/tokens/IToken.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+/**
+ * @title ERC20 IToken contract
+ * @dev Implementation of the interest bearing token for the OneStake Finance
+ * @author OneStake Finance
+ */
+
+contract IToken {
+}
---
+++ contracts/tokens/OST.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+/**
+ * @title ERC20 OST Token Contract
+ * @dev OneStake Finance's governance token
+ * @author OneStake Finance
+ */
+
+contract OST {
+}
