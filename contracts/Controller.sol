c8b0a10304684d2224ba0abe349b3eaafde7d980
SmartCoding51 <topstar51@outlook.com>
2022-05-14 02:04:07 +0900

    add natspec docs for controller & IUSD vault contracts

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,5 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+/**
+ * @title Controller contract
+ * @dev Gatekeeping interface between vaults and strategies, and oversees communication and fund flows.
+ * @author OneStake Finance
+ **/
+
 contract Controller {
 }
--- contracts/vaults/IUSD.sol
+++ contracts/vaults/IUSD.sol
@@ -1,6 +1,12 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+/**
+ * @title IUSD vault contract
+ * @dev consolidates stable coins in one pool.
+ * @author OneStake Finance
+ */
+
 contract IUSD {
 
 }
