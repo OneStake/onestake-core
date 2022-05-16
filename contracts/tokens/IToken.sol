3b46ddd79370c02855e8fab8c38a4aedbdecb7be
SmartCoding51 <topstar51@outlook.com>
2022-05-17 04:38:24 +0900

    temp commit for iToken contract

--- contracts/tokens/IToken.sol
+++ contracts/tokens/IToken.sol
@@ -7,5 +7,33 @@ pragma solidity ^0.8.0;
  * @author OneStake Finance
  */
 
-contract IToken {
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract IToken is Initializable, ERC20Upgradeable {
+    /**
+    * @dev Initializes the iToken
+    * @param _iTokenName The name of the aToken
+    * @param _iTokenSymbol The symbol of the aToken
+    */
+    function initialize(
+        string calldata _iTokenName,
+        string calldata _iTokenSymbol
+    ) external initializer {
+        __ERC20_init(_iTokenName, _iTokenSymbol);
+    }
+
+    function mint(
+        address _user,
+        uint256 _amount
+    ) external {
+        _mint(_user, _amount);
+    }
+
+    function burn(
+        address _user,
+        uint256 _amount
+    ) external {
+        _burn(_user, _amount);
+    }
 }
