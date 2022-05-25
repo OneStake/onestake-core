00051ff2a1b5b8de01903a3d6a0b98d6d4c61bbf
SmartCoding51 <topstar51@outlook.com>
2022-05-26 00:53:18 +0900

    add comptroller interface

---
+++ contracts/interfaces/external/IComptroller.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+interface IComptroller {
+    /**
+    * @notice Claim all the COMP accrued by specific holders in specific markets 
+    * for their supplies and/or borrows
+     */ 
+    function claimComp(
+        address[] memory holders,
+        address[] memory cTokens,
+        bool borrowers,
+        bool suppliers
+    ) external;
+}
