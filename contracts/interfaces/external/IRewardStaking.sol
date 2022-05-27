e9381adbe2baa098d3076aa7a131c5878c7e386e
SmartCoding51 <topstar51@outlook.com>
2022-05-28 00:25:10 +0900

    add reward & staking interface for convex

---
+++ contracts/interfaces/external/IRewardStaking.sol
@@ -0,0 +1,32 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface for Convex Reward & Staking
+ * https://github.com/convex-eth/platform/blob/main/contracts/contracts/interfaces/IRewardStaking.sol
+ */
+
+interface IRewardStaking {
+    function stakeFor(address, uint256) external;
+
+    function stake(uint256) external;
+
+    function withdraw(uint256 amount, bool claim) external;
+
+    function withdrawAndUnwrap(uint256 amount, bool claim) external;
+
+    function earned(address account) external view returns (uint256);
+
+    function getReward() external;
+
+    function getReward(address _account, bool _claimExtras) external;
+
+    function extraRewardsLength() external returns (uint256);
+
+    function extraRewards(uint256 _pid) external returns (address);
+
+    function rewardToken() external returns (address);
+
+    function balanceOf(address account) external view returns (uint256);
+}
