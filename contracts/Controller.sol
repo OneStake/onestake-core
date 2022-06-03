856b59890df633e92ce67dd5609d83f43e930c4e
SmartCoding51 <topstar51@outlook.com>
2022-06-04 02:22:01 +0900

    add controller contract

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,5 +1,11 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+import "./interfaces/IIUSD.sol";
 
 /**
  * @title Controller contract
@@ -7,5 +13,28 @@ pragma solidity ^0.8.0;
  * @author OneStake Finance
  **/
 
-contract Controller {
+contract Controller is Initializable, ReentrancyGuardUpgradeable {
+    IIUSD public iUSDVault;
+
+    modifier onlyUSDVault() {
+        require(msg.sender == address(iUSDVault), "Not iUSD vault");
+        _;
+    }
+
+    function initialize() external initializer {
+        __ReentrancyGuard_init();
+    }
+
+    function depositUSD(
+        address _user,
+        address _asset,
+        uint256 _amount
+    ) external nonReentrant onlyUSDVault {
+        IERC20Upgradeable(_asset).transferFrom(_user, address(this), _amount);
+    }
+
+    function setUSDVault(address _iUSDVault) external {
+        require(_iUSDVault != address(0), "Must specify iUSD vault address");
+        iUSDVault = IIUSD(_iUSDVault);
+    }
 }
